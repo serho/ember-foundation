bcf0be9c851d5e88e4621373205166e222c7efe1
Joshua Forisha <jforisha@softlayer.com>
2014-09-15 17:01:12 -0500

    Remove need to include application view mixin

--- app/initializers/ember-foundation.js
+++ app/initializers/ember-foundation.js
@@ -3,5 +3,8 @@ export default {
 
   initialize: function (container, app) {
     app.inject('component:f-breadcrumbs', 'router', 'router:main');
+    window.addEventListener('load', function () {
+      $(document).foundation();
+    });
   }
 };
--- tests/dummy/app/views/application.js
+++
@@ -1,4 +0,0 @@
-import Ember from 'ember';
-import EnableFoundation from 'ember-foundation/mixins/enable-foundation';
-
-export default Ember.View.extend(EnableFoundation);
