49b357b925ef0f575daf15878bb619710e3a3fe3
Joshua Forisha <jforisha@softlayer.com>
2014-09-19 14:09:16 -0500

    Remove unnecessary helper and register util

--- addon/utils/register-helpers.js
+++
@@ -1,6 +0,0 @@
-import Ember from 'ember';
-import times from '../helpers/times';
-
-export default function () {
-  Ember.Handlebars.registerHelper('times', times);
-}
--- app/initializers/ember-foundation.js
+++ app/initializers/ember-foundation.js
@@ -1,14 +1,10 @@
 /* globals $ */
 
-import registerHelpers from 'ember-foundation/utils/register-helpers';
-
 export default {
   name: 'ember-foundation',
 
   initialize: function (container, app) {
     app.inject('component:f-breadcrumbs', 'router', 'router:main');
-
-    registerHelpers();
 
     window.addEventListener('load', function () {
       $(document).foundation();
